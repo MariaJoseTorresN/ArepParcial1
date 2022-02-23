d1422d41483904057d48417f61e4e6e4cad8dfc5
unknown <maria.torres-n@labinfo.is.escuelaing.edu.co>
2022-02-23 14:26:48 -0500

    adelanto

---
+++ Parcial/src/main/java/edu/escuelaing/arep/App.java
@@ -0,0 +1,19 @@
+package edu.escuelaing.arep;
+
+import java.net.URL;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        boolean running = true;
+        while (running) {
+            
+            
+        }
+    }
+}
